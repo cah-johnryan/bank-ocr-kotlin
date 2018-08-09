5f9bc82fef8f2a833ca58be1d334395a9968effd
AddictedToBattlestar <andy@Ryan-MacBook-Pro.local>
2018-08-09 20:47:24 +0900

    Tests for the other numbers scaled up.

--- src/test/kotlin/com/nenaner/katas/bankocr/BankOcrControllerTests.kt
+++ src/test/kotlin/com/nenaner/katas/bankocr/BankOcrControllerTests.kt
@@ -4,35 +4,77 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.function.Executable
 
-object BankOcrControllerTests: Spek({
+object BankOcrControllerTests : Spek({
     given("a bank OCR Controller") {
         val bankOcrController = BankOcrController()
         on("scanning an image of a single image of a number from the page") {
-            it("should return 1") {
-                assertEquals(1, bankOcrController.scan(
-                        "   \n" +
-                                "  |\n" +
-                                "  |"))
-            }
-        }
-        on("scanning an image of a \"2\" from the page") {
-            val result = bankOcrController.scan(
-                    " _ \n" +
-                                " _|\n" +
-                                "|_ ")
-            it("should return 2") {
-                assertEquals(2, result)
-            }
-        }
-        on("scanning an image of a \"3\" from the page") {
-            val result = bankOcrController.scan(
-                    " _ \n" +
-                                " _|\n" +
-                                " _|")
-            it("should return 3") {
-                assertEquals(3, result)
+            it("should be able to interpret the single number provided") {
+                assertAll(
+                        Executable {
+                            assertEquals(1, bankOcrController.scan(
+                                    "   \n" +
+                                            "  |\n" +
+                                            "  |"))
+                        },
+                        Executable {
+                            assertEquals(2, bankOcrController.scan(
+                                    " _ \n" +
+                                            " _|\n" +
+                                            "|_ "))
+                        },
+                        Executable {
+                            assertEquals(3, bankOcrController.scan(
+                                    " _ \n" +
+                                            " _|\n" +
+                                            " _|"))
+                        },
+                        Executable {
+                            assertEquals(4, bankOcrController.scan(
+                                    "   \n" +
+                                            "|_|\n" +
+                                            "  |"))
+                        },
+                        Executable {
+                            assertEquals(5, bankOcrController.scan(
+                                    " _ \n" +
+                                            "|_ \n" +
+                                            " _|"))
+                        },
+                        Executable {
+                            assertEquals(6, bankOcrController.scan(
+                                    " _ \n" +
+                                            "|_ \n" +
+                                            "|_|"))
+                        },
+                        Executable {
+                            assertEquals(7, bankOcrController.scan(
+                                    " _ \n" +
+                                            "  |\n" +
+                                            "  |"))
+                        },
+                        Executable {
+                            assertEquals(8, bankOcrController.scan(
+                                    " _ \n" +
+                                            "|_|\n" +
+                                            "|_|"))
+                        },
+                        Executable {
+                            assertEquals(9, bankOcrController.scan(
+                                    " _ \n" +
+                                            "|_|\n" +
+                                            " _|"))
+                        },
+                        Executable {
+                            assertEquals(0, bankOcrController.scan(
+                                    " _ \n" +
+                                            "| |\n" +
+                                            "|_|"))
+                        }
+                )
             }
         }
     }
