27ce523d2884fd8238285bd66dabf023809fcea9
AddictedToBattlestar <andy@Ryan-MacBook-Pro.local>
2018-08-09 20:35:25 +0900

    Adding changes for 3 ... and all other numbers.

--- src/main/kotlin/com/nenaner/katas/bankocr/BankOcrController.kt
+++ src/main/kotlin/com/nenaner/katas/bankocr/BankOcrController.kt
@@ -1,10 +1,43 @@
 package com.nenaner.katas.bankocr
 
 class BankOcrController {
-    val imageOfOne = "   \n" +
-            "  |\n" +
-            "  |"
+    private val imageMap = mapOf(
+                    "   \n" +
+                    "  |\n" +
+                    "  |" to 1,
+                    " _ \n" +
+                    " _|\n" +
+                    "|_ " to 2,
+                    " _ \n" +
+                    " _|\n" +
+                    " _|" to 3,
+                    "   \n" +
+                    "|_|\n" +
+                    "  |" to 4,
+                    " _ \n" +
+                    "|_ \n" +
+                    " _|" to 5,
+                    " _ \n" +
+                    "|_ \n" +
+                    "|_|" to 6,
+                    " _ \n" +
+                    "  |\n" +
+                    "  |" to 7,
+                    " _ \n" +
+                    "|_|\n" +
+                    "|_|" to 8,
+                    " _ \n" +
+                    "|_|\n" +
+                    " _|" to 9,
+                    " _ \n" +
+                    "| |\n" +
+                    "|_|" to 0
+            )
     fun scan(sourceImage: String): Int? {
-        return if (imageOfOne.equals(sourceImage)) 1 else 2
+        return getNumberFromImage(sourceImage)
+    }
+
+    private fun getNumberFromImage(sourceImage: String): Int? {
+        return imageMap[sourceImage]
     }
 }
--- src/test/kotlin/com/nenaner/katas/bankocr/BankOcrControllerTests.kt
+++ src/test/kotlin/com/nenaner/katas/bankocr/BankOcrControllerTests.kt
@@ -9,13 +9,12 @@ import org.junit.jupiter.api.Assertions.assertEquals
 object BankOcrControllerTests: Spek({
     given("a bank OCR Controller") {
         val bankOcrController = BankOcrController()
-        on("scanning an image of a \"1\" from the page") {
-            val result = bankOcrController.scan(
-                    "   \n" +
-                                "  |\n" +
-                                "  |")
+        on("scanning an image of a single image of a number from the page") {
             it("should return 1") {
-                assertEquals(1, result)
+                assertEquals(1, bankOcrController.scan(
+                        "   \n" +
+                                "  |\n" +
+                                "  |"))
             }
         }
         on("scanning an image of a \"2\" from the page") {
@@ -25,6 +24,15 @@ object BankOcrControllerTests: Spek({
                                 "|_ ")
             it("should return 2") {
                 assertEquals(2, result)
+            }
+        }
+        on("scanning an image of a \"3\" from the page") {
+            val result = bankOcrController.scan(
+                    " _ \n" +
+                                " _|\n" +
+                                " _|")
+            it("should return 3") {
+                assertEquals(3, result)
             }
         }
     }
